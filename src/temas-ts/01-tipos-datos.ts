370223611b169bd9e9a3330b377c97fae99244b2
YaelHerberth <yael.herbert@outlook.com>
2022-09-10 12:05:33 -0500

    subida1

---
+++ src/temas-ts/01-tipos-datos.ts
@@ -0,0 +1,18 @@
+
+
+console.log('Hola Mundo!');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+var t1 : number | string = 3;
+let t2 : number | string = 2;
+let activo : boolean = false;
+var t3 = 666;
+
+t1='www';
+
+t2='www';
+
+console.log(t1);
