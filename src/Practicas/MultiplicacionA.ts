1210a30f2111c6e0f61a20d967c46c0d54a2f6e9
YaelHerberth <yael.herbert@outlook.com>
2022-09-17 22:22:44 -0500

    PracticaMultiplicacion

---
+++ src/Practicas/MultiplicacionA.ts
@@ -0,0 +1,11 @@
+//multiplicacion de a x b con sumas
+
+import { Operacion } from "./MultiplicacionB";
+
+let operacion : Operacion;
+
+operacion=new Operacion();
+operacion.setNumber(1);
+operacion.SetNumber2(5)
+
+operacion.Calcular();
---
+++ src/Practicas/MultiplicacionB.ts
@@ -0,0 +1,31 @@
+export class Operacion{
+    private numeroA : number;
+    private numeroB : number;
+    private total : number
+
+    public setNumber(x:number){
+        this.numeroA=0;
+        this.numeroA=x;
+    }
+
+    public SetNumber2(y:number){
+        this.numeroB=0;
+        this.numeroB=y;
+    }
+
+    public Calcular(){
+        this.calculo();
+    }
+
+    public Resultado(){
+        console.log('Resultado: '+(this.total));
+    }
+    private calculo(){
+        this.total=0;
+        for(let i=0;i<this.numeroB;i++){
+            this.total+=this.numeroA;
+            console.log('La operacion:'+(this.total-this.numeroA)+"="+(this.total));
+        }
+        this.Resultado();
+    }
+}
