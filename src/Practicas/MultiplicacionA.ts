19d466138be2dde40b6ad00d58983a75f69d50e2
YaelHerberth <yael.herbert@outlook.com>
2022-09-17 22:45:15 -0500

    Correccion

--- src/Practicas/MultiplicacionA.ts
+++ src/Practicas/MultiplicacionA.ts
@@ -5,7 +5,7 @@ import { Operacion } from "./MultiplicacionB";
 let operacion : Operacion;
 
 operacion=new Operacion();
-operacion.setNumber(8);
-operacion.SetNumber2(10)
+operacion.setNumberA(8);
+operacion.SetNumberB(10)
 
 operacion.Calcular();
